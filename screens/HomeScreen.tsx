b420b1c79ce1093b2ca710131f8aa2ffb54d1deb
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2022-04-19 09:19:29 -0500

    Create basic Root screens

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,15 +1,28 @@
-import { StyleSheet } from 'react-native';
+import { AccessibilityInfo, StyleSheet, Image } from 'react-native';
+// import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
-import EditScreenInfo from '../components/EditScreenInfo';
+// import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Tab One</Text>
-			<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-			<EditScreenInfo path="/screens/HomeScreen.tsx" />
+		<View style={styles.container} accessibilityLiveRegion='polite'>
+			<View>
+				<Text style={styles.title}>Bienvenido Usuario</Text>
+			</View>
+
+			{/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
+
+			<View style={styles.component}>
+				<Text style={styles.subtitle}>Caracterización de Finca</Text>
+
+				<Text>
+					Añada las fotos de sus cultivos para continuar con la carga de datos
+				</Text>
+
+				<View accessibilityRole='progressbar'></View>
+			</View>
 		</View>
 	);
 }
@@ -18,9 +31,16 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: 'center',
-		justifyContent: 'center',
+		// justifyContent: 'center',
+	},
+	component: {
+		backgroundColor: "#280907",
 	},
 	title: {
+		fontSize: 50,
+		fontWeight: 'bold',
+	},
+	subtitle: {
 		fontSize: 20,
 		fontWeight: 'bold',
 	},
@@ -28,5 +48,7 @@ const styles = StyleSheet.create({
 		marginVertical: 30,
 		height: 1,
 		width: '80%',
+	},
+	section: {
 	},
 });
